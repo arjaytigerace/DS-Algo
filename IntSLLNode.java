f8e0bef55b812da96733dc6ac374110cba280024
arjaytigerace <raeniel2098@gmail.com>
2017-08-10 23:12:05 +0800

    Update IntSLLNode.java

--- IntSLLNode.java
+++ IntSLLNode.java
@@ -3,21 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prelimexam_cornelio;
+package midlab2_cornelio;
 
 /**
  *
  * @author raeni
  */
-public class IntSLLNode {
+public class IntSLNode {
     int info;
-    IntSLLNode next;
-    IntSLLNode(int i){
+    IntSLNode next;
+    IntSLNode(int i){
         this(i, null);
     }
-    public IntSLLNode(int i, IntSLLNode n){
+    public IntSLNode(int i, IntSLNode n){
         info = i;
         next = n;
     }
-    
+
+  
 }
